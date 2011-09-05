6bd1e4f84cd27959d41f3426afa16bd41f78b3da
Sanders Kleinfeld <sanders@oreilly.com>
2011-09-05 10:31:01 -0400

    Putting JS in separate file.

---
+++ glossary/glossary.js
@@ -0,0 +1,27 @@
+window.addEventListener('load', eventWindowLoaded, false);	
+
+function eventWindowLoaded() {
+    set_up_audio();
+}
+
+function set_up_audio() {
+    var epub_audio = document.getElementById("epub");
+    var mobi_audio = document.getElementById("mobi");
+    var pdf_audio = document.getElementById("pdf");
+    // Add play button functionality
+    var epub_play_button = document.getElementById("epub_button");
+    var mobi_play_button = document.getElementById("mobi_button");
+    var pdf_play_button = document.getElementById("pdf_button");
+    epub_play_button.addEventListener("click", play_epub, false);
+    mobi_play_button.addEventListener("click", play_mobi, false);
+    pdf_play_button.addEventListener("click", play_pdf, false);
+    function play_epub() {
+	epub_audio.play();
+    }
+    function play_mobi() {
+	mobi_audio.play();
+    }
+    function play_pdf() {
+	pdf_audio.play();
+    }
+}
