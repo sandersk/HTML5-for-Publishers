e775cdbb50fb8be4af7738bd6ed1c6df76b52bf4
Sanders Kleinfeld <sanders@oreilly.com>
2012-01-30 22:01:06 -0500

    Revised regexes for a, b, and c values; added solver functionality.

--- mathml/mathml.js
+++ mathml/mathml.js
@@ -54,33 +54,48 @@ function MathMLApp(){
 	var aValue = $("#input_a").val();
 	var bValue = $("#input_b").val();
 	var cValue = $("#input_c").val();
-	var naturalNumPattern = /^[1-9][0-9]*$/;
+	// Regex pattern for acceptable a values, which must be integers > 0
+	var aPattern = /^-?[1-9][0-9]*$/;
+	// Regex pattern for acceptable b/c values, which must be integers >= 0
+	var bcPattern = /^(0|(-?[1-9][0-9]*))$/;
 	if (aValue == "") {
 	    errorText += "Enter integer for <em>a</em><br/>";
 	    // Reset aValue to default of "a"
 	    aValue = "a";
-	} else if (!(aValue.match(naturalNumPattern))) {
+	    // Reset answer to "?"
+	    $("#equation_solution").text("?");
+	} else if (!(aValue.match(aPattern))) {
 	    errorText += "Invalid value for <em>a</em>: " + aValue + "<br/>";
 	    // Reset aValue to default of "a"
 	    aValue = "a";
+	    // Reset answer to "?"
+	    $("#equation_solution").text("?");
 	}
 	if (bValue == "") {
 	    errorText += "Enter integer for <em>b</em><br/>";
 	    // Reset bValue to default of "b"
 	    bValue = "b";
-	} else if (!(bValue.match(naturalNumPattern))) {
+	    // Reset answer to "?"
+	    $("#equation_solution").text("?");
+	} else if (!(bValue.match(bcPattern))) {
 	    errorText += "Invalid value for <em>b</em>: " + bValue + "<br/>";
 	    // Reset bValue to default of "b"
 	    bValue = "b";
+	    // Reset answer to "?"
+	    $("#equation_solution").text("?");
 	}
 	if (cValue == "") {
 	    errorText += "Enter integer for <em>c</em><br/>";
 	    // Reset cValue to default of "c"
 	    cValue = "c";
-	} else if (!(cValue.match(naturalNumPattern))) {
+	    // Reset answer to "?"
+	    $("#equation_solution").text("?");
+	} else if (!(cValue.match(bcPattern))) {
 	    errorText += "Invalid value for <em>c</em>: " + cValue + "<br/>";
 	    // Reset cValue to default of "c"
 	    cValue = "c";
+	    // Reset answer to "?"
+	    $("#equation_solution").text("?");
 	}
 	$("math .a").each(function() {
 	        $(this).text(aValue);
@@ -92,6 +107,28 @@ function MathMLApp(){
 	        $(this).text(cValue);
 	    });
 	$("#error_log").html(errorText);
+	// If error text is now empty, it means valid numbers have been entered for a, b, and c.
+	// Go ahead and solve the quadratic equation
+	if (errorText == "") {
+	    solve_quadratic_equation(aValue, bValue, cValue);
+	}
+    }
+
+    function solve_quadratic_equation(aValue, bValue, cValue) {
+	var discriminant = (bValue*bValue) - 4*aValue*cValue;
+	var equationSolution = "";
+	if (discriminant < 0) {
+	    // No solution to equation; display null set
+	    $("#equation_solution").text("No Solution");
+	} else if (discriminant == 0) {
+	    equationSolution = -bValue/(2*aValue);
+	    $("#equation_solution").text(equationSolution);
+	} else {
+	    var solution1 = (-bValue - Math.sqrt(discriminant))/(2*aValue);
+	    var solution2 = (-bValue + Math.sqrt(discriminant))/(2*aValue);
+	    equationSolution = String(solution1) + ", " + String(solution2);
+	    $("#equation_solution").text(equationSolution);
+	}
     }
      
 
