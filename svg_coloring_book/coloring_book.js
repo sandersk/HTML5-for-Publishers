c8c339ed2e8275cb94653ddd3bfc8143d42a1a85
Sanders Kleinfeld <sanders@oreilly.com>
2013-01-12 16:50:28 -0500

    Logic to color the drawing.

--- svg_coloring_book/coloring_book.js
+++ svg_coloring_book/coloring_book.js
@@ -3,15 +3,17 @@ window.addEventListener('load', eventWindowLoaded, false);
 function eventWindowLoaded() {
 
     // Add click events for colorable portions of drawing
-    $('.colorable').bind("click", function(event) { 
-        $(this).attr("fill", "red"); 
+    $('.colorable').bind("click", function(event) {
+	// Set the fill of clicked portion of drawing to the color chosen in palette below
+	color_chosen = $("#color_chosen").html()
+        $(this).attr("fill", color_chosen); 
     });
 
     // Add click events for color palette
     $('.color_choice').bind("click", function(event) {
        // Get button id, which is the color name 
-       color_selected = $(this).attr("id")
+       color_chosen = $(this).attr("id")
        // Set color_chosen text to the name of color clicked
-       $("#color_chosen").html(color_selected); 
+       $("#color_chosen").html(color_chosen); 
     });
 }
