d9dabb5589a7209d5349c7a809ba4440c8d797ba
Sanders Kleinfeld <sanders@oreilly.com>
2013-01-12 16:47:09 -0500

    Adding color palette button logic.

--- svg_coloring_book/coloring_book.js
+++ svg_coloring_book/coloring_book.js
@@ -1,7 +1,17 @@
 window.addEventListener('load', eventWindowLoaded, false);	
 
 function eventWindowLoaded() {
+
+    // Add click events for colorable portions of drawing
     $('.colorable').bind("click", function(event) { 
         $(this).attr("fill", "red"); 
+    });
+
+    // Add click events for color palette
+    $('.color_choice').bind("click", function(event) {
+       // Get button id, which is the color name 
+       color_selected = $(this).attr("id")
+       // Set color_chosen text to the name of color clicked
+       $("#color_chosen").html(color_selected); 
     });
 }
