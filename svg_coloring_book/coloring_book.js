0ac42dcd8b96579be4a932c802e70e3f08c9a3d3
Sanders Kleinfeld <sanders@oreilly.com>
2013-01-13 09:38:56 -0500

    Syntax cleanup

--- svg_coloring_book/coloring_book.js
+++ svg_coloring_book/coloring_book.js
@@ -21,14 +21,14 @@ function add_coloring_book_events() {
     // Add click events for colorable portions of drawing
     $('.colorable').bind("click", function(event) {
 	// Set the fill of clicked portion of drawing to the color chosen in palette below
-	color_chosen = $("#color_chosen").html()
+	color_chosen = $("#color_chosen").html();
         $(this).attr("fill", color_chosen); 
     });
 
     // Add click events for color palette
     $('.color_choice').bind("click", function(event) {
        // Get button id, which is the color name 
-       color_chosen = $(this).attr("id")
+       color_chosen = $(this).attr("id");
        // Set color_chosen text to the name of color clicked
        $("#color_chosen").html(color_chosen); 
     });
